6c216a2eae56ef7c7b09b23e38ed9b77462dcdab
ozcotech <ozkancomert@aof.anadolu.edu.tr>
2022-02-20 13:39:38 +0300

    simpleCal.java added.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basit Hesap Makinesi 
+### Basit Hesap Makinesi Patika Ödevi
+*Problem sadece ilk sayı küçük olduğunda ikinci sayıya bölünememesi, yani bölünüyor da sonuç 0 çıkıyor, o yüzden basit diyoruz...*
---
+++ simpleCal.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class simpleCal{
+
+public static void main(String[] args){
+
+  int no1, no2, islem;
+  
+  Scanner input = new Scanner(System.in);
+
+  System.out.print("Birinci Sayıyı Giriniz: ");
+
+  no1 = input.nextInt();
+
+  System.out.println("Birinci Sayınız: " + no1);
+
+  System.out.print("İkinci Sayıyı Giriniz: ");
+
+  no2 = input.nextInt();
+
+  System.out.println("İkinci Sayınız: " + no2);
+
+  System.out.println("*****************************\nHesaplama Yapmak için;\n Toplama için 1\n Çıkarma için 2\n Çarpma için 3\n Bölme için 4\n*****************************");
+
+  System.out.println("Lütfen İşlem Numarasını Giriniz: ");
+
+  islem = input.nextInt();
+  
+  switch (islem) {
+    case 1:
+      System.out.println("1- Toplama İşlemi - Sonuc: " + (no1 + no2));
+      break;
+
+      case 2:
+      System.out.println("2- Çıkarma İşlemi - Sonuc: " + (no1 - no2));
+      break;
+
+      case 3:
+      System.out.println("3- Çarpma İşlemi - Sonuc: " + (no1 * no2));
+      break;
+
+      case 4:
+      System.out.println("4- Bölme İşlemi - Sonuc: " + (no1 / no2));
+      break;
+
+    default:
+      System.out.println("Geçersiz İşlem Numarası Girdiniz..");
+      break;
+  }
+  
+
+  input.close();
+
+}
+
+}
